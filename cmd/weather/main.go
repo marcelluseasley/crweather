b2df7442e9aac8e0ee15e5b0e03f0e9993baf487
Marcellus Easley <marcellus.easley@gmail.com>
2024-06-26 06:00:17 -0400

    add main and template for browser rendering

---
+++ cmd/weather/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"crweather/internal/weather/db/sqlite3"
+	"crweather/internal/weather/rest"
+	"crweather/internal/weather/service"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	weatherStorage, err := sqlite3.NewDBStore()
+	if err != nil {
+		log.Fatalf("could not initialize the database: %v", err)
+	}
+	weatherService := service.NewWeatherService(weatherStorage)
+	handler := rest.NewWeatherHandler(weatherService)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", handler.Ping)
+
+	r.Route("/weather", func(r chi.Router) {
+		r.Get("/{input}", handler.GetWeather)
+		r.Get("/{lat}/{long}", handler.GetWeather)
+	})
+
+	log.Println("listening on port 3000")
+	http.ListenAndServe(":3000", r)
+}
